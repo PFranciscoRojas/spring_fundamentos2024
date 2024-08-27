d66bf7db7e2b0004127d71a15e0a8c9fce94649e
pfranciscorojas <franciscorojas92@gmail.com>
2024-08-27 17:21:08 -0500

    estructura proyecto

---
+++ src/main/java/com/example/libreria/controller/SaludoController.java
@@ -0,0 +1,17 @@
+package com.example.libreria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/saludar")
+public class SaludoController {
+    
+    @GetMapping("/recibirsaludo")  
+    public String saludo(){
+        return "holi :D ";
+    }
+}
