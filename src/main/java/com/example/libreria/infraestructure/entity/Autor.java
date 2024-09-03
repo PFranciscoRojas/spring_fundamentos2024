322f22644a1bbf6cb60c724fa3ddaae540333402
pfranciscorojas <franciscorojas92@gmail.com>
2024-09-03 17:22:29 -0500

    add entities

--- src/main/java/com/example/libreria/infraestructure/entity/Autor.java
+++ src/main/java/com/example/libreria/infraestructure/entity/Autor.java
@@ -1,40 +1,53 @@
 package com.example.libreria.infraestructure.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
-@Table(name="autores")
+@Table(name = "autores")
 public class Autor {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_autor")
-    private Integer idAutor;
+    private Long idAutor;
+
     private String nombre;
+
     private String nacionalidad;
 
-    public Integer getIdAutor() {
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Libro> libros;
+
+    public Long getIdAutor() {
         return idAutor;
     }
-    public void setIdAutor(Integer idAutor) {
+
+    public void setIdAutor(Long idAutor) {
         this.idAutor = idAutor;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getNacionalidad() {
         return nacionalidad;
     }
+
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
 
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
 }
--- src/main/java/com/example/libreria/infraestructure/entity/Compra.java
+++ src/main/java/com/example/libreria/infraestructure/entity/Compra.java
@@ -1,5 +1,26 @@
 package com.example.libreria.infraestructure.entity;
 
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "Compras")
 public class Compra {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_libro", nullable = false)
+    private Libro libro;
+
+    private Date fecha;
+
+    // Getters y setters
 }
